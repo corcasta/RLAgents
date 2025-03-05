5663c376c85ee94fc5889951da57ff2f44f9d5d9
oscar <corcasta@outlook.com>
2025-03-05 10:53:37 +0100

    Changes in training loop logic and adding an env wrapper for packing 4 frames

--- dqn_demo.py
+++ dqn_demo.py
@@ -13,9 +13,9 @@ from agents import DQNAgent, state_preprocessor, reward_clipping
 
 RUNS_PATH          = os.path.dirname(__file__) + "/runs"
 DEVICE             ="cuda" if torch.cuda.is_available() else "cpu"
-EPOCHS             = 500 #100
-EPISODES           = 10_000
-FIRST_N_FRAMES     = 500_000 #1_000_000
+EPOCHS             = 50_000 #100
+EPISODES           = 100
+FIRST_N_FRAMES     = 4_000_000
 MEMORY_BUFFER_SIZE = 100_000
 MAX_STEPS          = 100_000
 SKIPPED_FRAMES     = 4
@@ -28,7 +28,7 @@ def main():
     # Environment SETUP
     gym.register_envs(ale_py)
     env = gym.make("ALE/Breakout-v5", max_episode_steps=MAX_STEPS, obs_type="grayscale")#, render_mode="human")
-   
+    env = gym.wrappers.FrameStackObservation(env, 4)
 
     # Agent SETUP
     agent = DQNAgent(INPUT_SHAPE, 4, MEMORY_BUFFER_SIZE, 32, 1, device=DEVICE)
@@ -45,15 +45,17 @@ def main():
     for epoch in range(EPOCHS):
         episode_pg_bar.n = 0
         epoch_total_reward = 0
+        epoch_loss = 0
 
         # Episodes loop 
         for episode in range(EPISODES):
             # At the start of every epoch the environment is reseted.
             episode_total_reward = 0
+            episode_loss = 0
             dead = False
             frame_buffer = [] 
             obs, info = env.reset()
-            processed_obs = state_preprocessor(obs, frame_buffer, FRAME_BUFFER_SIZE) * FRAME_BUFFER_SIZE
+            processed_obs = state_preprocessor(obs)
             
             while not dead:
                 action = agent.get_action(processed_obs)
@@ -65,40 +67,46 @@ def main():
                 
                 # Data preprocessing
                 reward = reward_clipping(reward)
-                processed_next_obs = state_preprocessor(next_obs, frame_buffer, FRAME_BUFFER_SIZE)  
+                processed_next_obs = state_preprocessor(next_obs)  
                 experience = (processed_obs, action, reward, processed_next_obs, dead)
                 
                 # Storing data in buffer and moving to next iter
                 agent.store_experience(experience)
                 processed_obs = copy.deepcopy(processed_next_obs)
-                
-                                
+                                    
                 # Adjusting epsilon from greedy strategy
                 writer.add_scalar("Epsilon Evolution", agent.epsilon, frame_number)    
                 if frame_number <= FIRST_N_FRAMES:
                     agent.epsilon -= (1-0.1)/FIRST_N_FRAMES
-                else:
-                    agent.epsilon = 0.05
+                #else:
+                #    agent.epsilon = 0.05
+                
+                # Update agent weights
+                episode_loss += agent.update()
                 
             epoch_total_reward += episode_total_reward
-
+            epoch_loss += episode_loss
+            
             # Updating tensorboard log
             writer.add_scalar("Total_Reward/Episode", episode_total_reward, episode)
+            writer.add_scalar("Loss/Episode", episode_loss, episode)
                 
             # Update TQDM bar    
-            episode_pg_bar.set_postfix({"total_reward": episode_total_reward})
+            episode_pg_bar.set_postfix({"total_reward": episode_total_reward, "loss": episode_loss})
             episode_pg_bar.update()
             
-            # Update agent weights
-            agent.update()
+
             
         # Updating tensorboard log
-        writer.add_scalar("Total_Reward/Epoch", epoch_total_reward, epoch)
+        writer.add_scalars("Epoch", {"Total_Reward": epoch_total_reward,
+                                     "Avg_Reward": epoch_total_reward/EPISODES,
+                                     "Loss": epoch_loss,
+                                     "Avg_Loss": epoch_loss/EPISODES}, epoch)
         writer.flush() 
         
         # Update TQDM bar
         epoch_pg_bar.set_description(desc=f"Epoch")
-        epoch_pg_bar.set_postfix({"total_reward": epoch_total_reward})
+        epoch_pg_bar.set_postfix({"total_reward": epoch_total_reward, "avg_loss": epoch_loss/EPISODES})
         epoch_pg_bar.update()
         
     writer.close()    
